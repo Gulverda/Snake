80f7de68abf923dd95fd3d3fa27d467a1d07d077
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-10-17 20:44:35 +0400

    Update Snake.js

--- src/Components/Snake.js
+++ src/Components/Snake.js
@@ -5,17 +5,27 @@ const BlockSize = 25;
 const Rows = 20;
 const Cols = 20;
 
-const SnakeX = BlockSize * 5;
-const SnakeY = BlockSize * 5;
-
 const Canvas = styled.canvas`
   border: 1px solid #000;
 `;
 
 const MyComponent = () => {
   const boardRef = useRef(null);
-  const [foodX, setFoodX] = useState(BlockSize * 10);
-  const [foodY, setFoodY] = useState(BlockSize * 10);
+  const [snake, setSnake] = useState([{ x: BlockSize * 5, y: BlockSize * 5 }]);
+  const [velocityX, setVelocityX] = useState(0);
+  const [velocityY, setVelocityY] = useState(0);
+  const [score, setScore] = useState(0);
+  const [foodPosition, setFoodPosition] = useState(generateRandomPosition());
+
+  function generateRandomPosition() {
+    const foodX = Math.floor(Math.random() * Cols) * BlockSize;
+    const foodY = Math.floor(Math.random() * Rows) * BlockSize;
+    return { x: foodX, y: foodY };
+  }
+
+  function isCollisionWithFood(newSnakeHead, foodPos) {
+    return newSnakeHead.x === foodPos.x && newSnakeHead.y === foodPos.y;
+  }
 
   useEffect(() => {
     const board = boardRef.current;
@@ -28,33 +38,86 @@ const MyComponent = () => {
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
 
-    // Update function
-    function update() {
-      context.clearRect(0, 0, board.width, board.height);
+    // Game Loop
+    function gameLoop() {
+      const newSnake = [...snake];
+      const newSnakeHead = {
+        x: snake[0].x + velocityX * BlockSize,
+        y: snake[0].y + velocityY * BlockSize,
+      };
+      newSnake.unshift(newSnakeHead);
 
-      context.fillStyle = "black";
-      context.fillRect(0, 0, board.width, board.height);
+      // Check for collisions with food
+      if (isCollisionWithFood(newSnakeHead, foodPosition)) {
+        // Increase the score
+        setScore(score + 1);
 
-      context.fillStyle = "lime";
-      context.fillRect(SnakeX, SnakeY, BlockSize, BlockSize);
+        // Generate a new position for the food
+        const newFoodPosition = generateRandomPosition();
+        setFoodPosition(newFoodPosition);
+      } else {
+        newSnake.pop();
+      }
 
-      context.fillStyle = "red";
-      context.fillRect(foodX, foodY, BlockSize, BlockSize);
+      // Check for collisions with the boundaries
+      if (
+        newSnakeHead.x >= 0 &&
+        newSnakeHead.y >= 0 &&
+        newSnakeHead.x < board.width &&
+        newSnakeHead.y < board.height
+      ) {
+        setSnake(newSnake);
+
+        context.clearRect(0, 0, board.width, board.height);
+
+        context.fillStyle = "black";
+        context.fillRect(0, 0, board.width, board.height);
+
+        context.fillStyle = "lime";
+        snake.forEach((segment) => {
+          context.fillRect(segment.x, segment.y, BlockSize, BlockSize);
+        });
+
+        context.fillStyle = "red";
+        context.fillRect(foodPosition.x, foodPosition.y, BlockSize, BlockSize);
+      }
+
+      requestAnimationFrame(gameLoop);
     }
 
-    // PlaceFood function
-    function placeFood() {
-      const newFoodX = Math.floor(Math.random() * Cols) * BlockSize;
-      const newFoodY = Math.floor(Math.random() * Rows) * BlockSize;
-      setFoodX(newFoodX);
-      setFoodY(newFoodY);
+    // Arrow key handlers
+    function handleKeyDown(e) {
+      if (e.code === "ArrowUp" && velocityY !== 1) {
+        setVelocityX(0);
+        setVelocityY(-1);
+      } else if (e.code === "ArrowDown" && velocityY !== -1) {
+        setVelocityX(0);
+        setVelocityY(1);
+      } else if (e.code === "ArrowLeft" && velocityX !== 1) {
+        setVelocityX(-1);
+        setVelocityY(0);
+      } else if (e.code === "ArrowRight" && velocityX !== -1) {
+        setVelocityX(1);
+        setVelocityY(0);
+      }
     }
 
-    placeFood();
-    update();
-  }, [foodX, foodY]);
+    // Attach arrow key event listeners
+    document.addEventListener("keydown", handleKeyDown);
 
-  return <Canvas id="board" ref={boardRef}></Canvas>;
+    requestAnimationFrame(gameLoop);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [snake, velocityX, velocityY, score, foodPosition]);
+
+  return (
+    <div>
+      <div>Score: {score}</div>
+      <Canvas id="board" ref={boardRef}></Canvas>
+    </div>
+  );
 };
 
 export default MyComponent;
