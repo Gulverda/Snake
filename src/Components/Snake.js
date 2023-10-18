4bb8334df8571b77d34b52189f3af6944231a8bc
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-10-18 23:01:20 +0400

    Added Yellow ball and +2 on size

--- src/Components/Snake.js
+++ src/Components/Snake.js
@@ -1,123 +1,189 @@
-import React, { useEffect, useRef, useState } from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-const BlockSize = 25;
-const Rows = 20;
-const Cols = 20;
+const numRows = 20;
+const numCols = 20;
 
-const Canvas = styled.canvas`
-  border: 1px solid #000;
+const GameGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${numCols}, 25px);
+  grid-template-rows: repeat(${numRows}, 25px);
+  background-color: #030303;
 `;
 
-const MyComponent = () => {
-  const boardRef = useRef(null);
-  const [snake, setSnake] = useState([{ x: BlockSize * 5, y: BlockSize * 5 }]);
-  const [velocityX, setVelocityX] = useState(0);
-  const [velocityY, setVelocityY] = useState(0);
-  const [score, setScore] = useState(0);
-  const [foodPosition, setFoodPosition] = useState(generateRandomPosition());
+const Cell = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-  function generateRandomPosition() {
-    const foodX = Math.floor(Math.random() * Cols) * BlockSize;
-    const foodY = Math.floor(Math.random() * Rows) * BlockSize;
-    return { x: foodX, y: foodY };
-  }
+const SnakeSegment = styled(Cell)`
+  background-color: green;
+  border: 1px solid wheat;
+`;
 
-  function isCollisionWithFood(newSnakeHead, foodPos) {
-    return newSnakeHead.x === foodPos.x && newSnakeHead.y === foodPos.y;
-  }
+const Food = styled(Cell)`
+  background-color: red;
+  display: flex;
+  align-items: center;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  margin-top: 3.5px;
+  margin-left: 4px;
+`;
 
-  useEffect(() => {
-    const board = boardRef.current;
-    const context = board.getContext("2d");
+const YellowBall = styled(Cell)`
+  background-color: yellow;
+  border-radius: 50%;
+`;
 
-    board.height = Rows * BlockSize;
-    board.width = Cols * BlockSize;
-
-    // Drawing
-    context.fillStyle = "black";
-    context.fillRect(0, 0, board.width, board.height);
-
-    // Game Loop
-    function gameLoop() {
-      const newSnake = [...snake];
-      const newSnakeHead = {
-        x: snake[0].x + velocityX * BlockSize,
-        y: snake[0].y + velocityY * BlockSize,
-      };
-      newSnake.unshift(newSnakeHead);
-
-      // Check for collisions with food
-      if (isCollisionWithFood(newSnakeHead, foodPosition)) {
-        // Increase the score
-        setScore(score + 1);
-
-        // Generate a new position for the food
-        const newFoodPosition = generateRandomPosition();
-        setFoodPosition(newFoodPosition);
-      } else {
-        newSnake.pop();
-      }
-
-      // Check for collisions with the boundaries
-      if (
-        newSnakeHead.x >= 0 &&
-        newSnakeHead.y >= 0 &&
-        newSnakeHead.x < board.width &&
-        newSnakeHead.y < board.height
-      ) {
-        setSnake(newSnake);
-
-        context.clearRect(0, 0, board.width, board.height);
-
-        context.fillStyle = "black";
-        context.fillRect(0, 0, board.width, board.height);
-
-        context.fillStyle = "lime";
-        snake.forEach((segment) => {
-          context.fillRect(segment.x, segment.y, BlockSize, BlockSize);
-        });
-
-        context.fillStyle = "red";
-        context.fillRect(foodPosition.x, foodPosition.y, BlockSize, BlockSize);
-      }
-
-      requestAnimationFrame(gameLoop);
-    }
-
-    // Arrow key handlers
-    function handleKeyDown(e) {
-      if (e.code === "ArrowUp" && velocityY !== 1) {
-        setVelocityX(0);
-        setVelocityY(-1);
-      } else if (e.code === "ArrowDown" && velocityY !== -1) {
-        setVelocityX(0);
-        setVelocityY(1);
-      } else if (e.code === "ArrowLeft" && velocityX !== 1) {
-        setVelocityX(-1);
-        setVelocityY(0);
-      } else if (e.code === "ArrowRight" && velocityX !== -1) {
-        setVelocityX(1);
-        setVelocityY(0);
-      }
-    }
-
-    // Attach arrow key event listeners
-    document.addEventListener("keydown", handleKeyDown);
-
-    requestAnimationFrame(gameLoop);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      snake: [{ row: 5, col: 5 }],
+      food: { row: 10, col: 10 },
+      yellowBall: null,
+      direction: 'RIGHT',
+      score: 0,
+      gameStarted: false,
     };
-  }, [snake, velocityX, velocityY, score, foodPosition]);
+  }
 
-  return (
-    <div>
-      <div>Score: {score}</div>
-      <Canvas id="board" ref={boardRef}></Canvas>
-    </div>
-  );
-};
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+    this.gameInterval = setInterval(this.moveSnake, 200);
+    this.spawnYellowBall(); // Start spawning yellow balls
+  }
 
-export default MyComponent;
+  componentWillUnmount() {
+    clearInterval(this.gameInterval);
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = (event) => {
+    if (!this.state.gameStarted) {
+      this.setState({ gameStarted: true });
+      this.gameInterval = setInterval(this.moveSnake, 200);
+    }
+
+    switch (event.key) {
+      case 'ArrowUp':
+        this.setState({ direction: 'UP' });
+        break;
+      case 'ArrowDown':
+        this.setState({ direction: 'DOWN' });
+        break;
+      case 'ArrowLeft':
+        this.setState({ direction: 'LEFT' });
+        break;
+      case 'ArrowRight':
+        this.setState({ direction: 'RIGHT' });
+        break;
+      default:
+        break;
+    }
+  };
+
+  moveSnake = () => {
+    const { snake, direction, food, score, yellowBall } = this.state;
+    const head = Object.assign({}, snake[0]);
+
+    switch (direction) {
+      case 'UP':
+        head.row -= 1;
+        break;
+      case 'DOWN':
+        head.row += 1;
+        break;
+      case 'LEFT':
+        head.col -= 1;
+        break;
+      case 'RIGHT':
+        head.col += 1;
+        break;
+      default:
+        break;
+    }
+
+    const newSnake = [head, ...snake];
+
+    if (this.isCollisionWithFood(head, food)) {
+      this.generateFood();
+      this.setState({ score: score + 1 });
+    } else if (this.isCollisionWithFood(head, yellowBall)) {
+      this.setState({ yellowBall: null, score: score + 2 }); // +2 for yellow ball
+      this.scheduleYellowBallSpawn(); // Schedule the next yellow ball spawn
+    } else {
+      newSnake.pop();
+    }
+
+    this.setState({ snake: newSnake });
+  };
+
+  isCollisionWithFood(head, food) {
+    return food && head.row === food.row && head.col === food.col;
+  }
+
+  generateFood = () => {
+    const newFood = {
+      row: Math.floor(Math.random() * numRows),
+      col: Math.floor(Math.random() * numCols),
+    };
+    this.setState({ food: newFood });
+  };
+
+  spawnYellowBall = () => {
+    this.scheduleYellowBallSpawn();
+  };
+
+  scheduleYellowBallSpawn = () => {
+    const delay = Math.floor(Math.random() * 16) * 2000 + 5000; // Random delay between 5 and 20 seconds
+    setTimeout(() => {
+      const yellowBall = {
+        row: Math.floor(Math.random() * numRows),
+        col: Math.floor(Math.random() * numCols),
+      };
+      this.setState({ yellowBall });
+      setTimeout(() => {
+        this.setState({ yellowBall: null });
+        this.scheduleYellowBallSpawn();
+      }, 6000); // Yellow ball disappears after 3 seconds
+    }, delay);
+  };
+
+  render() {
+    const { snake, food, yellowBall, score, gameStarted } = this.state;
+
+    const grid = [];
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        const isSnakeSegment = snake.some(
+          (segment) => segment.row === i && segment.col === j
+        );
+        const isFood = this.isCollisionWithFood({ row: i, col: j }, food);
+        const isYellowBall = this.isCollisionWithFood({ row: i, col: j }, yellowBall);
+
+        const CellComponent = isSnakeSegment ? SnakeSegment : isFood ? Food : isYellowBall ? YellowBall : Cell;
+
+        grid.push(<CellComponent key={`${i}-${j}`} />);
+      }
+    }
+
+    return (
+      <div>
+        {gameStarted ? (
+          <div>
+            <GameGrid>{grid}</GameGrid>
+            <div>Score: {score}</div>
+          </div>
+        ) : (
+          <div>Press an arrow key to start the game.</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
